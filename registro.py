3ac6795ae1d89d4ad1e9e7b6e4d043fe27b01d6c
maximo-cobeaga <maximocobeaga@gmail.com>
2022-03-27 16:58:45 -0300

    Mini sistema a base de un input

--- registro.py
+++ registro.py
@@ -3,11 +3,40 @@ from time import strptime
 from traceback import format_tb
 
 
-in_deseado = "14:00"
-out_deseado = "21:00"
+formato_hora = "%H:%M"
 
-def conversor(a):
-    hora_formateada = datetime.strptime(a, '%H:%M')
-    print(hora_formateada)
+# Conversion a tipo datetime 
+in_ = datetime.strptime("14:00", formato_hora)
+out_ = datetime.strptime("21:00", formato_hora)
 
-conversor("13:30")
+#Recortes de las variables
+in_formated = in_.strftime(formato_hora)
+out_formated = out_.strftime(formato_hora)
+
+#Entrada de datos
+user_in = input('\n¿A que hora ingresaste?   -->  ')
+user_out = input('\n¿A que hora saliste?     -->  ')
+
+#Conversor a tipo datetime de la entrada de datos
+user_in_f = datetime.strptime(user_in,formato_hora)
+user_out_f = datetime.strptime(user_out,formato_hora)
+
+#Recorte de las variables 
+user_in_formated = user_in_f.strftime(formato_hora)
+user_out_formated = user_out_f.strftime(formato_hora)
+
+#Condicional para comprobar horarios de entrada
+if user_in_formated != in_formated:
+    print('\nEstas en al hora equivocada\n')
+else: 
+    print('\nEstas en la hora correcta\n')
+
+#Condicional para comprobar horarios de salida
+if user_out_formated != out_formated:
+    print('Estas en al hora equivocada\n')
+else: 
+    print('Estas en la hora correcta\n')
+
+
+
+
